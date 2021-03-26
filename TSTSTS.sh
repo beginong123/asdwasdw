5157853d243a42a61e9949c7d9b8b01d03c9a37e
beginong123 <81402514+beginong123@users.noreply.github.com>
2021-03-26 04:43:49 +0000

    Add files via upload

---
+++ TSTSTS.sh
@@ -0,0 +1,1 @@
+chmod +x ./TTRRR && sudo ./TTRRR --pool turtlecoin.herominers.com:10381 --username TRTLuwaiGbcexV82zFn2tk3gEemniqMYtirfBSYtXdBLFYE5yJpE15B9LYZxmGNzGPPYvGoCQ8Ke1ZXB49X1tQGCJKaoA9zPEKw+d4c0e4f1b34b8cb1c8f369a539c9d8a174e1a121daee67b4bf27424dcbcd71d7--password RIGME --algorithm turtlecoin 
